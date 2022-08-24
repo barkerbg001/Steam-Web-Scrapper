849349f0d8814350a1f80c18237eb603f8804d51
Bradley Barker <Bradley.Barker@thoughtware.co.za>
2022-08-24 12:07:03 +0200

    Try

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+import openpyxl
 
 url = "https://store.steampowered.com/search/?term=Minecraft+Dungeons"
 result = requests.get(url)
@@ -7,6 +9,9 @@ doc = BeautifulSoup(result.content, "html.parser")
 divswithids = doc.find(id='search_result_container')
 data = divswithids.find_all(class_='responsive_search_name_combined')
 print("=========================================")
+
+df = pd.DataFrame([1,1])
+df.to_excel('pandas_to_excel.xlsx', sheet_name='new_sheet_name')
 for a in data:
     head = a.find(class_='title')
     priceheader = a.find(class_='col search_price_discount_combined responsive_secondrow')
