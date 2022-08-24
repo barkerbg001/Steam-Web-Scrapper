20be13cdc76b43b1c7dce38a604a6aa9e4d59865
Bradley Barker <Bradley.Barker@thoughtware.co.za>
2022-08-24 14:44:58 +0200

    letter

--- main.py
+++ main.py
@@ -3,25 +3,27 @@ import requests
 import pandas as pd
 import openpyxl
 
-url = "https://store.steampowered.com/search/?term=Minecraft+Dungeons"
-result = requests.get(url)
-doc = BeautifulSoup(result.content, "html.parser")
-divswithids = doc.find(id='search_result_container')
-data = divswithids.find_all(class_='responsive_search_name_combined')
-print("=========================================")
+letters = ["a","b","c","d","e"]
+for b in letters:
+    url = "https://store.steampowered.com/search/?term=" + b
+    result = requests.get(url)
+    doc = BeautifulSoup(result.content, "html.parser")
+    divswithids = doc.find(id='search_result_container')
+    data = divswithids.find_all(class_='responsive_search_name_combined')
+    print("=========================================")
 
-table = []
+    table = []
 
-for a in data:
-    head = a.find(class_='title')
-    priceheader = a.find(class_='col search_price_discount_combined responsive_secondrow')
-    price = priceheader.find(class_='col search_price responsive_secondrow')
-    try:
-        row= [head.string, price.string.strip()]
-        print(row)
-        table.append(row)
-    except:
-        print("Woops")
+    for a in data:
+        head = a.find(class_='title')
+        priceheader = a.find(class_='col search_price_discount_combined responsive_secondrow')
+        price = priceheader.find(class_='col search_price responsive_secondrow')
+        try:
+            row= [head.string, price.string.strip()]
+            print(row)
+            table.append(row)
+        except:
+            print("Woops")
 
-df = pd.DataFrame(table,columns=['a', 'b'])
-df.to_excel('steam.xlsx', index=False, header=False)
+    df = pd.DataFrame(table,columns=['a', 'b'])
+    df.to_excel('steam.xlsx', index=False, header=False)
